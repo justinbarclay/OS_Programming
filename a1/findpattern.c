98649747f2111a371fbf5e985e53365b84d71e90
mackenzie <mackenziebligh@gmail.com>
2017-01-20 21:15:16 -0700

    Setup file and create short write up

---
+++ a1/findpattern.c
@@ -0,0 +1,17 @@
+/*******************************************************************
+ * CMPUT 379 Assignment 1
+ * Due:
+ *
+ * Group: Mackenzie Bligh & Justin Barclay
+ * CCIDs: bligh & jbarclay
+ * *****************************************************************
+ * Notes: this section should be removed
+ * - findpattern() cannot send any output to stdout */
+
+unsigned int findpattern (unsigned char *pattern, unsigned int patlength,\
+        struct patmatch *locations, unsigned int loclength){
+    int pattern_occurrances = 0;  
+    /*  Body of function goes here */
+    return pattern_occurrances;
+}
+
