88b7a6cd1dcd55d7e6185712095b510512e31d50
mackenzie <mackenziebligh@gmail.com>
2017-01-28 15:07:12 -0700

    Begin implementing find pattern

--- a1/findpattern.c
+++ a1/findpattern.c
@@ -1,5 +1,4 @@
 #include "findpattern.h"
-#include <stdlib.h>
 /*******************************************************************
  * CMPUT 379 Assignment 1
  * Due:
@@ -12,14 +11,20 @@
  * - Must impliment a way to print the patmatch struct
  *   */
 
+
 unsigned int findpattern (unsigned char *pattern, unsigned int patlength,\
         struct patmatch *locations, unsigned int loclength){
     int pattern_occurrances = 0;  
     /*  Body of function goes here */
-    char* currentAddress = 0x0;
+    char *currentAddress = 0x0;
+    char *startAddress = 0x0;
     bool read;
     bool write;
     
+    struct memory_range range;
+    range.start_address = startAddress;
+    range.stop_address = currentAddress;
+
     while(currentAddress < 0xffffffff){
         
         read = canRead(currentAddress);
@@ -27,21 +32,23 @@ unsigned int findpattern (unsigned char *pattern, unsigned int patlength,\
         currentAddress++;
         
         if(read){
-            // If the address can be read
-            
-            // Create a patmatch struct
-            struct patmatch *match;
-            match = malloc(sizeof(struct patmatch));
-
+            //create a description of the allowable block of memory
+            range.permission = 0;
             if(write){
-            // And the address can be written to 
+            range.permission = 1;
             }
-            // The address can only be read
         } else {
             //if can't read go to next page
+            find_match_in_range(range);
             currentAddress = nextPage(currentAddress);
         }
     }
     return pattern_occurrances;
 }
+/*  Function body */
+void find_match_in_range(struct memory_range range, unsigned char *pattern, unsigned int patlength){
+    void *mem;
+     
+    mem = memmem(range.start_address, (range.stop_address- range.start_address), pattern, patlength);
 
+}
--- a1/findpattern.h
+++ a1/findpattern.h
@@ -1,4 +1,7 @@
 #include "readmem.h"
+#include <stdlib.h>
+#include <string.h>
+
 #define MEM_RW 0
 #define MEM_RO 1
 
@@ -7,5 +10,14 @@ struct patmatch {
     unsigned char mode; /*  MEM_RW or MEM_RO*/
 };
 
+struct memory_range{
+    char *start_address ;    
+    char *stop_address;
+    int permission;
+};
+
 unsigned int findpattern (unsigned char *pattern, unsigned int patlength,\
         struct patmatch *locations, unsigned int loclength);
+
+void find_match_in_range(struct memory_range range, unsigned char *pattern, unsigned int patlength);
+
