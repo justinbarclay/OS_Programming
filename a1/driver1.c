815f94adc20b5376111922000db72e57867150c1
Justin Barclay <justincbarclay@gmail.com>
2017-02-04 23:09:12 -0700

    Add linked list version of patterns, hopefully give us better ability to spread items through memory

--- a1/driver1.c
+++ a1/driver1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 #include "findpattern.h"
 
 /* test1                              #Should be test1 or test2 or test3 corresponding to three invocations  */
@@ -28,20 +29,17 @@
 /* 0xFF139    MEM_RO    C      #separated by tab (\t), U:unchanged, C: only mode is changed, N: new match */
 
 /* 0XFFEB9   MEM_RW    N     #separated by tab (\t), U:unchanged, C: only mode is changed, N: new match */
+struct node{
+    char* pattern;
+    struct node* next;
+} typedef LinkedList;
 
 void report(int testNum, unsigned int length, struct patmatch* test1, struct patmatch* test2);
 
+LinkedList* addNode(LinkedList* head, unsigned char* pattern, int length);
+void freeNodes(LinkedList* head);
 int main(){
     unsigned char pattern[10] = {'X', 'Y', '1', '0', 'A', 'd', 't', '@', ' ', '8'};
-    char* pattern2 = (char *) calloc(1, sizeof(pattern));
-    char* pattern3 = (char *) calloc(1, sizeof(pattern));
-    char* pattern4 = (char *) calloc(1, sizeof(pattern));
-    char* pattern5 = (char *) calloc(1, sizeof(pattern));
-    char* pattern6 = (char *) calloc(1, sizeof(pattern));
-    char* pattern7 = (char *) calloc(1, sizeof(pattern));
-    char* pattern8 = (char *) calloc(1, sizeof(pattern));
-    char* pattern9 = (char *) calloc(1, sizeof(pattern));
-    char* pattern10 = (char *) calloc(1, sizeof(pattern));
     
     struct patmatch* test1 = calloc(100, sizeof(struct patmatch));
     struct patmatch* test2 = calloc(100, sizeof(struct patmatch));
@@ -56,33 +54,25 @@ int main(){
 
     report(1, found, test1, 0);
     // Copy pattern over
-    memcpy(pattern2, pattern, sizeof(pattern));
-    memcpy(pattern3, pattern, sizeof(pattern));
-    memcpy(pattern3, pattern, sizeof(pattern));
-    memcpy(pattern4, pattern, sizeof(pattern));
-    memcpy(pattern5, pattern, sizeof(pattern));
-    memcpy(pattern6, pattern, sizeof(pattern));
-    memcpy(pattern7, pattern, sizeof(pattern));
-    memcpy(pattern8, pattern, sizeof(pattern));
-    memcpy(pattern9, pattern, sizeof(pattern));
-    memcpy(pattern10, pattern, sizeof(pattern));
 
-    
+    LinkedList* node1 = addNode(0, &pattern[0], sizeof(pattern));
+    LinkedList* node2 = addNode(node1, &pattern[0], sizeof(pattern));
+    LinkedList* node3 = addNode(node2, &pattern[0], sizeof(pattern));
+    LinkedList* node4 = addNode(node3, &pattern[0], sizeof(pattern));
+    LinkedList* node5 = addNode(node4, &pattern[0], sizeof(pattern));
+    LinkedList* node6 = addNode(node5, &pattern[0], sizeof(pattern));
+    LinkedList* node7 = addNode(node6, &pattern[0], sizeof(pattern));
+    LinkedList* node8 = addNode(node7, &pattern[0], sizeof(pattern));
+    LinkedList* node9 = addNode(node8, &pattern[0], sizeof(pattern));
+
+    printf("Memprotect %i\n", mprotect(node9, sizeof(LinkedList)*2, PROT_READ));
     found = findpattern((unsigned char*) pattern, 10, test2, 100);
 
     report(2, found, test1, test2);
 
 
     // Free malloc variables
-    free(pattern2);
-    free(pattern3);
-    free(pattern4);
-    free(pattern5);
-    free(pattern6);
-    free(pattern7);
-    free(pattern8);
-    free(pattern9);
-    free(pattern10);
+    free(node9);
     free(test1);
     free(test2);
     
@@ -104,3 +94,25 @@ void report(int testNum, unsigned int length, struct patmatch* test1, struct pat
     }
 }
 
+LinkedList* addNode(LinkedList* head, unsigned char* pattern, int length){
+   //create a link
+   LinkedList* next = (LinkedList*) malloc(sizeof(LinkedList));
+   next->pattern = calloc(length, sizeof(char));
+   memcpy(next->pattern, pattern, length);
+   next->next = head;
+   
+   //point it to old first node
+   head = next;
+
+   return next;
+}
+
+void freeNodes(LinkedList* head){
+    LinkedList* next;
+    while(head->next != 0){
+        free(head->pattern);
+        next = head->next;
+        head = head->next;
+        free(next);        
+    }
+}
