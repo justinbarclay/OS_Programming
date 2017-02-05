d53a575ac7c0728246550ffdee9df78ba4a55c31
mackenzie <mackenziebligh@gmail.com>
2017-02-05 16:01:49 -0700

    Attempt implementling cli, is broken

--- a1/driver3.c
+++ a1/driver3.c
@@ -8,36 +8,46 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 void report(int testNum, unsigned int length, struct patmatch* test1, struct patmatch* test2);
+size_t getStringLength(char* pattern);
 
-int main(){
-    unsigned char pattern[10] = {'h', '\xff'};
+int main(int argc, char *argv[]){
     // Declare variables for use in driver
     int fileDescriptor;
     FILE *fp;
     int found;
-    
+    int patLength = 0;
+
     // Allocate arrays to pass as locations to findpattern
     struct patmatch* test1 = calloc(100, sizeof(struct patmatch));
     struct patmatch* test2 = calloc(100, sizeof(struct patmatch));
+    if(argc == 2){
+        patLength = getStringLength(argv[1]);
+    } else{
+        printf("Invalid input, please ensure that only one command line argument is passed in.\n");
+        return -1;
+    }
+    //
+    //pattern is patLength to get the terminating null character
+    unsigned char pattern[patLength+1];
+    strncpy(pattern, argv[1], patLength+1);
 
     // Beginning of test format printing
     fprintf(stdout, "test3\n");
     fprintf(stdout, "mmap is used to place a file descriptor of testing.txt (created by this program) into memory\n\n");
 
     // Converting pattern to unsigned char* as it is a pointer to first element in a list
-    // Make first pass and report 
+    // Make first pass and report
     found = findpattern((unsigned char*) pattern, 10, test1, 100);
     report(1, found, test1, 0);
-   
+
     // Create a file containing the pattern, and write to it to ensure pattern exists
     fp = fopen("driver3Mapping.txt", "w+");
     fprintf(fp, "%s", pattern);
-    fclose(fp);
     fp = NULL;
 
     // Open file descriptor to file you just created
     fileDescriptor = open("testing.txt", O_WRONLY);
-    
+
     // Map pattern into memory using mmap(2)
     void *p = mmap(NULL, sizeof(fp), PROT_READ, MAP_SHARED, fileDescriptor, 0);
 
@@ -52,6 +62,7 @@ int main(){
     /* printf("Memprotect %i\n", mprotect((void *) nodeBoundary, getpagesize(), PROT_READ)); */
 }
 
+/*  Function bodies */
 void report(int testNum, unsigned int length, struct patmatch* test1, struct patmatch* test2){
 //    printf("Report %i matches found\n", length);
     size_t i = 0;
@@ -69,3 +80,13 @@ void report(int testNum, unsigned int length, struct patmatch* test1, struct pat
     fprintf(stdout, "\n");
 }
 
+
+size_t getStringLength(char* pattern){
+    char null = '\0';
+
+    size_t i=0;
+    while(pattern[i] != null){
+        i++;
+    }
+    return i;
+}
