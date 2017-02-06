bf7f6783b7ca18e59d4e8c08448a4676ef50f87d
mackenzie <mackenziebligh@gmail.com>
2017-02-05 17:46:40 -0700

    Finish driver 3

--- a1/driver3.c
+++ a1/driver3.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[]){
         printf("Invalid input, please ensure that only one command line argument is passed in.\n");
         return -1;
     }
-    
+
     //pattern is patLength to get the terminating null character
     unsigned char pattern[patLength+1];
     strncpy(pattern, argv[1], patLength+1);
@@ -35,7 +35,7 @@ int main(int argc, char *argv[]){
     fprintf(stdout, "test3\n");
     fprintf(stdout, "mmap is used to place a file descriptor of testing.txt (created by this program) into memory\n\n");
 
-    
+
     // Create a file containing the pattern, and write to it to ensure pattern exists
     fp = fopen("driver3Mapping.txt", "w+");
     fprintf(fp, "%s", pattern);
@@ -44,22 +44,24 @@ int main(int argc, char *argv[]){
 
     // Get filesize
     struct stat st;
-    stat("driver3Mapping.txt", &st);
 
     // Open file descriptor to file you just created
-    fileDescriptor = open("driver3Mapping.txt", O_WRONLY);
+    fileDescriptor = open("driver3Mapping.txt", O_RDWR, S_IRUSR | S_IWUSR);
 
+    fstat(fileDescriptor, &st);
     // Map pattern into memory using mmap(2)
-    void *p = mmap(&fileDescriptor,st.st_size ,PROT_WRITE, MAP_SHARED, fileDescriptor, 0);
-    
+    void *p = mmap(NULL,st.st_size , PROT_READ, MAP_SHARED, fileDescriptor, 0);
+    void *addr = p;
     // Converting pattern to unsigned char* as it is a pointer to first element in a list
     // Make first pass and report
     found = findpattern((unsigned char*) pattern, 10, test1, 100);
     report(1, found, test1, 0);
 
     // Make remapping of memory from read
-     p = mmap(&fileDescriptor,st.st_size , PROT_READ, MAP_SHARED, fileDescriptor, 0);
-
+    p =  mmap(addr,st.st_size , PROT_WRITE, MAP_FIXED | MAP_PRIVATE, fileDescriptor, 0);
+    if(p == MAP_FAILED){
+        printf("FAIL\n");
+    }
     // Make second pass and report
     found = findpattern((unsigned char*) pattern, 10, test2, 100);
     report(2, found, test1, test2);
