3c6d7101c1076a7323d611005c165e47a3686365
Justin Barclay <justincbarclay@gmail.com>
2017-02-17 17:44:01 -0700

    Make client.c multithreaded

--- a2/client.c
+++ a2/client.c
@@ -18,20 +18,60 @@
 #include <netdb.h>
 #include <strings.h>
 #include <string.h>
+#include <pthread.h>
 #define	MY_PORT	2222
+
+struct thread_info {
+    struct hostent *host;
+    int* err;
+};
+
+void readFromHost(void *info);
 
 int main(int argc, char *argv[]){
     int s, number;
     char message[512];
     struct sockaddr_in server;
     struct hostent *host;
-    char sendBuffer[20];
+    
     host = gethostbyname("localhost");
+    int err;
 
     if(host == NULL){
         perror("Client: cannot get host description");
         exit(-1);
     }
+
+    pthread_t read_host_thread;
+    struct thread_info info;
+    info.host = host;
+    info.err = &err;
+    if(pthread_create(&read_host_thread, NULL, readFromHost,(void *) &info)) {
+
+        fprintf(stderr, "Error creating thread\n");
+        return 1;
+
+    }
+    while(1){
+        if(pthread_join(read_host_thread, NULL)) {
+
+            fprintf(stderr, "Error joining thread\n");
+            return 2;
+ 
+        }
+    }
+    return 0;
+}
+
+void readFromHost(void *info){
+    //Read from a given socket and if an error code occurs it inserts it into err and cleans itself up
+    int s;
+    char message[512];
+    struct sockaddr_in server;
+    struct thread_info *tInfo = (struct thread_info*) info;
+    struct hostent *host = tInfo->host;
+    int *err = tInfo->err;
+
     while(1){
         // Create new socket
         s = socket(AF_INET, SOCK_STREAM, 0);
@@ -39,7 +79,6 @@ int main(int argc, char *argv[]){
         // Check for failed socket
         if (s < 0) {
             perror ("Client: cannot open socket");
-            exit (-2);
         }
         bzero(&server, sizeof(server)); // zeroize socket struct
         // Copy server address into socket struct
@@ -47,25 +86,13 @@ int main(int argc, char *argv[]){
 
         server.sin_family = host->h_addrtype;
         server.sin_port = htons (MY_PORT);
-
         if(connect (s, (struct sockaddr*) & server, sizeof (server))){
             perror("Client: cannot connect to server");
-            exit(-3);
         }
-
         read(s, message, 512); //Read socket; perhaps delete
-        /* // Wipe send buffer */
-        /* memset(sendBuffer, '', sizeof(sendBuffer)); */
-        /* // Write new info to sendbuffer */
-        /* sprintf(sendBuffer, "Socket %d\n",ntohl(number)); */
-        /* // Send */
-        /* send(s,sendBuffer, strlen(sendBuffer),0 ); close(s); // Close socket */
-
-        // Probably get rid of this bullshit
-        fprintf(stderr, "Process %d gets number %s\n", getpid (),
+        
+        fprintf(stderr, "Message recieved = %s\n",
                 message);
         sleep (2);
-
     }
-    return 0;
 }
