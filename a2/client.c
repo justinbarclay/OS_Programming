0b0fd8e80102d722e3362277fd69db4db7e680fb
Justin Barclay <justincbarclay@gmail.com>
2017-03-06 08:46:06 -0700

    Working, synchronous implementation of talking to server

--- a2/client.c
+++ a2/client.c
@@ -1,98 +1,212 @@
-/*******************************************************************
- * CMPUT 379 Assignment 2
- * Due:
- *
- * Group: Mackenzie Bligh & Justin Barclay
- * CCIDs: bligh & jbarclay
- * *****************************************************************
- * Notes: this section should be removed
- *   */
-
-/*  Imports*/
 #include <sys/types.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <netdb.h>
 #include <strings.h>
-#include <string.h>
-#include <pthread.h>
-#define	MY_PORT	2222
+#include <arpa/inet.h>
+#include <ncurses.h>
 
-struct thread_info {
-    struct hostent *host;
-    int* err;
-};
+#define	 MY_PORT  2222
 
-void readFromHost(void *info);
+/* ---------------------------------------------------------------------
+   This is a sample client program for the number server. The client and
+   the server need to run on the same machine.
+   --------------------------------------------------------------------- */
 
-int main(int argc, char *argv[]){
-    int s, number;
-    char message[512];
-    struct sockaddr_in server;
-    struct hostent *host;
+/* inspiration for how to use ncurses stolen from:
+   https://cboard.cprogramming.com/c-programming/172122-using-ncurses-1-output-window-updating-1-input-keyboard.html */
+/* more ncruses */
+/* http://www.tldp.org/HOWTO/html_single/NCURSES-Programming-HOWTO/#LETBEWINDOW */
+
+
+struct charBuffer {
+    char buffer[100];
+    int size;
+} typedef charBuffer;
+
+static charBuffer *output;
+static charBuffer *input;
+WINDOW *create_newwin(int height, int width, int starty, int startx);
+void destroy_win(WINDOW *local_win);
+
+
+void exampleSendRcv(WINDOW *w1, WINDOW *w2);
+
+void readFromSocket(int s, char* c, charBuffer *output);
+void getSocket(int *s);
+
+// This structure contains the current element that needs to be printed to screen
+
+int main(){
+    output = malloc(sizeof(charBuffer));
+    input = malloc(sizeof(charBuffer));
+    WINDOW *w1, *w2;
+    int row, column;
     
-    host = gethostbyname("localhost");
-    int err;
-
-    if(host == NULL){
-        perror("Client: cannot get host description");
-        exit(-1);
+    // Begin nCruses Mode
+    if (initscr() == NULL) {
+        fprintf(stderr, "Error: initscr()\n");
+        exit(EXIT_FAILURE);
     }
 
-    pthread_t read_host_thread;
-    struct thread_info info;
-    info.host = host;
-    info.err = &err;
-    if(pthread_create(&read_host_thread, NULL, readFromHost,(void *) &info)) {
+    keypad(stdscr, TRUE);  
+    noecho();              // Disable echoing of keyboard input
+    cbreak();              // disable line-buffering
+    timeout(100);          // wait 100 milliseconds for input
+    
+    getmaxyx(stdscr, row, column);
+    w1 = create_newwin(row-2, column, 0, 0);
+    w2 = create_newwin(2, column, row-2, 0);
 
-        fprintf(stderr, "Error creating thread\n");
-        return 1;
+    // This should be split off into two segments/ screen rendering and input
+    // network send recieve
+    // The network send recieve should be a pull interface, the system pulls whenever it is ready to send new stuff
+    exampleSendRcv(w1, w2);
 
-    }
-    while(1){
-        if(pthread_join(read_host_thread, NULL)) {
+    // Clean up
+    destroy_win(w1);
+    destroy_win(w2);
 
-            fprintf(stderr, "Error joining thread\n");
-            return 2;
- 
-        }
-    }
-    return 0;
+    free(output);
+    free(input);
 }
 
-void readFromHost(void *info){
-    //Read from a given socket and if an error code occurs it inserts it into err and cleans itself up
-    int s;
-    char message[512];
-    struct sockaddr_in server;
-    struct thread_info *tInfo = (struct thread_info*) info;
-    struct hostent *host = tInfo->host;
-    int *err = tInfo->err;
+void exampleSendRcv(WINDOW *w1, WINDOW *w2){
+    int	s, number;
+    char c[11];
+    
+    /* Vars for ncurses */
+    char buf[100] = {0}, *bufPointer = buf;
+    int ch, cnt = 0, n = 1;
+    // Zero out all bytes in character array c
+    bzero(c,11);
+    // assign 'M' to the last element of c
+    c[10] = 'M';
+    // assign 'A' to the first element of c
+    c[0] = 'A';
 
+    getSocket(&s);
+
+    // Send all 11 bytes of character array c to the server
+    // It is important to note that even the null terminating (zero valued) bytes
+    // are sent to the server. 
+    send(s,c,11,0);
+    readFromSocket(s, c, output);
+    refresh();
+    
     while(1){
-        // Create new socket
-        s = socket(AF_INET, SOCK_STREAM, 0);
-
-        // Check for failed socket
-        if (s < 0) {
-            perror ("Client: cannot open socket");
-        }
-        bzero(&server, sizeof(server)); // zeroize socket struct
-        // Copy server address into socket struct
-        bcopy (host->h_addr_list[0], & (server.sin_addr), host->h_length);
-
-        server.sin_family = host->h_addrtype;
-        server.sin_port = htons (MY_PORT);
-        if(connect (s, (struct sockaddr*) & server, sizeof (server))){
-            perror("Client: cannot connect to server");
-        }
-        read(s, message, 512); //Read socket; perhaps delete
+        wborder(w2, ' ', ' ', '_', ' ', ' ', ' ', ' ', ' ');
+        mvwprintw(w1, 0, 0, "%s\n",output->buffer);
+        mvwprintw(w2, 1, 0, "buf: %s", buf);
+        wnoutrefresh(w1);
+        wnoutrefresh(w2);
+        doupdate();
         
-        fprintf(stderr, "Message recieved = %s\n",
-                message);
-        sleep (2);
+        if ((ch = getch()) != ERR) {
+            if (ch == '\n') {
+
+                getSocket(&s); // Get new socket
+                send(s, buf, 11, 0);
+                readFromSocket(s, c, output);
+                close (s);
+                
+                *bufPointer = 0; // Set current char to 0
+                sscanf(buf, "%d", &n); //0 out buffer
+                bufPointer = buf;
+                *bufPointer = 0;
+            } else if (ch == 127) {
+                if (bufPointer > buf){
+                    *--bufPointer = 0;
+                }
+            } else if(ch == 'q') {
+                break;
+            } else if (bufPointer - buf < (long)sizeof buf - 1) {
+                *bufPointer++ = ch;
+                *bufPointer = 0;
+            }
+        }
+        // Erase the screen
+        werase(w1);
+        werase(w2);
     }
+    free(output);
+}
+
+void readFromSocket(int s, char* c, charBuffer *output){
+    // zero out each byte of the array before receiving from the server
+    bzero(c,11);
+
+    // Here the client wants to receive 7 bytes from the server, but the server
+    // only sends 5 bytes
+    recv(s,c,7,0);
+    memcpy(output->buffer, c,100);
+}
+
+void getSocket(int *s){
+    struct	sockaddr_in	server;
+
+    struct	hostent		*host;
+
+    host = gethostbyname ("localhost");
+
+    if (host == NULL) {
+        perror ("Client: cannot get host description");
+        exit (1);
+    }
+
+
+    *s = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (*s < 0) {
+        perror ("Client: cannot open socket");
+        exit (1);
+    }
+
+    bzero (&server, sizeof (server));
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_port = htons (MY_PORT);
+
+    if (connect (*s, (struct sockaddr*) & server, sizeof (server))) {
+        perror("Client: cannot connect to server");
+        exit (1);
+    }
+}
+
+
+WINDOW *create_newwin(int height, int width, int starty, int startx){
+    WINDOW *local_win;
+
+    local_win = newwin(height, width, starty, startx);
+    /* 0, 0 gives default characters 
+     * for the vertical and horizontal
+     * lines			*/
+        
+    wrefresh(local_win);		/* Show that box 		*/
+
+    return local_win;
+}
+
+void destroy_win(WINDOW *local_win){	
+    /* box(local_win, ' ', ' '); : This won't produce the desired
+     * result of erasing the window. It will leave it's four corners 
+     * and so an ugly remnant of window. 
+     */
+    wborder(local_win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
+    /* The parameters taken are 
+     * 1. win: the window on which to operate
+     * 2. ls: character to be used for the left side of the window 
+     * 3. rs: character to be used for the right side of the window 
+     * 4. ts: character to be used for the top side of the window 
+     * 5. bs: character to be used for the bottom side of the window 
+     * 6. tl: character to be used for the top left corner of the window 
+     * 7. tr: character to be used for the top right corner of the window 
+     * 8. bl: character to be used for the bottom left corner of the window 
+     * 9. br: character to be used for the bottom right corner of the window
+     */
+    wrefresh(local_win);
+    delwin(local_win);
 }
--- a2/server.c
+++ a2/server.c
@@ -1,14 +1,3 @@
-/*******************************************************************
- * CMPUT 379 Assignment 2
- * Due:
- *
- * Group: Mackenzie Bligh & Justin Barclay
- * CCIDs: bligh & jbarclay
- * *****************************************************************
- * Notes: this section should be removed
- *   */
-
-/*  Imports*/
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -19,47 +8,79 @@
 
 #define	MY_PORT	2222
 
-int main(int argc, char *argv[]){
-    int s, snew, fromlength, number, outnum;
-    char message[512];
-    memcpy(message, "hello", 5);
-    struct sockaddr_in master, from;
+/* ---------------------------------------------------------------------
+   This	is  a sample server which opens a stream socket and then awaits
+   requests coming from client processes.
+   --------------------------------------------------------------------- */
 
-    // Intitalize variables
-    number = 0;
+int main(int argc, char * argv[])
+{
+    int	sock, snew, fromlength, number, outnum;
 
-    s = socket(AF_INET, SOCK_STREAM, 0);
-    if(s < 0){
+    struct	sockaddr_in	master, from;
+    char c[11];
+
+
+    int i = 0;
+
+    sock = socket (AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
         perror ("Server: cannot open master socket");
-        exit (-1);
+        exit (1);
     }
 
     master.sin_family = AF_INET;
-    master.sin_addr.s_addr = INADDR_ANY;
+    master.sin_addr.s_addr = inet_addr("127.0.0.1");
     master.sin_port = htons (MY_PORT);
 
-    if(bind (s, (struct sockaddr*) &master, sizeof (master))){
-        perror("Server: cannot bind master socket");
+    if (bind (sock, (struct sockaddr*) &master, sizeof (master))) {
+        perror ("Server: cannot bind master socket");
         exit (1);
     }
-    // Start listening for socket
-    listen(s, 5);
+    char steve[7] = "Steven";
+    while(1){
+	listen (sock, 5);
+	fromlength = sizeof(from);
+	snew = accept (sock, (struct sockaddr*) & from, & fromlength);
+	if (snew < 0) {
+            perror ("Server: accept failed");
+            exit (1);
+	}
+	outnum = htonl (number);
+	
+	// Zero out all of the bytes in character array c
+	bzero(c,11);
 
-    while (1) {
-        fromlength = sizeof (from);
-        snew = accept(s, (struct sockaddr*) &from, (unsigned int*)&fromlength);
-        if (snew < 0) {
-            perror("Server: accept failed");
-            exit(-1);
-        }
-        // Setup for next listen
-        outnum = htonl(number);
-//        write(snew, &outnum, sizeof (outnum));
-        send(snew, "hello", 512, 0);
-        close(snew);
-        number++;
+	// Here we print off the values of character array c to show that
+	// each byte has an intial value of zero before receiving anything
+	// from the client.
+	printf("Before recieving from client\n--------------------------\n");
+	printf("Character array c has the following byte values:\n");
+	for (i = 0; i < 11; i++){
+            printf("c[%d] = %d\n",i,c[i]);
+	}
+
+	// Now we receive from the client, we specify that we would like 11 bytes
+	recv(snew,c,11,0);
+
+	// Print off the received bytes from the client as a string. 
+	// Next, print off the value of each byte to showcase that indeed
+	// 11 bytes were received from the client
+	printf("\nAfter receiving from client\n-------------------------\n");
+	printf("Printing character array c as a string is: %s\n",c);
+	printf("Character array c has the following byte values:\n");
+	for (i = 0; i < 11; i++){
+            printf("c[%d] = %d\n",i,c[i]);
+	}
+        
+	//copy the string "Stevens" into character array c
+	//strncpy(c,steve,7);
+	
+	//Send the first five bytes of character array c back to the client
+	//The client, however, wants to receive 7 bytes.
+	send(snew,c,5,0);
+
+	close (snew);
+	sleep(1);
     }
-
-
-    return 0;
 }
