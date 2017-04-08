fe7b5353f0c995023658eac35337fd53755d8066
Justin Barclay <justincbarclay@gmail.com>
2017-04-07 22:27:35 -0600

    fix small bug in adding to virtual memory

--- makefile
+++ makefile
@@ -2,7 +2,7 @@ CC = gcc
 linkedlist.o: linkedlist.c linkedlist.h
 	$(CC) -c linkedlist.c
 memory.o: linkedlist.o memory.c memory.h
-	$(CC) -c memory.c
+	$(CC) -c memory.c linkedlist.o
 simulation: simul.c linkedlist.o memory.o
 	$(CC) -o simul simul.c linkedlist.o memory.o 
 
--- memory.c
+++ memory.c
@@ -3,18 +3,18 @@
 int addToMemory(int pageNum, int pid, int POLICY, doubleLL* tlb, doubleLL* pageTable, node* frameBuffer[], doubleLL* virtualMemory){
     int frame;
     if((frame = nodeExists(pageNum, pid, tlb))>0 && POLICY){
-        printf("here");
+        
         node* item = frameBuffer[frame];
-        policyUpdateLRU(item, virtualMemory);
+        policyLRU(item, virtualMemory);
         return 0;
     } else if((frame = nodeExists(pageNum, pid, pageTable)) > 0 && POLICY){
         
         fflush(stdout);
         node* item = frameBuffer[frame];
-        policyUpdateLRU(item, virtualMemory);
+        policyLRU(item, virtualMemory);
         return 0;
     } else{
-        int frame = addToVirtualMemory(pageNum, pid,frameBuffer, virtualMemory);
+        int frame = addToVirtualMemory(pageNum, pid, frameBuffer, virtualMemory);
         addNewNode(pageNum, pid, frame, pageTable);
         addNewNode(pageNum, pid, frame, tlb);
         return 1;
@@ -24,7 +24,7 @@ int addToMemory(int pageNum, int pid, int POLICY, doubleLL* tlb, doubleLL* pageT
 int addToVirtualMemory(int pageNum,int pid, node* frameBuffer[], doubleLL* virtualMemory){
     // Function makes the assumption that frameBuffer
     int frame = 0;
-    if(virtualMemory->currentSize < virtualMemory->maxSize){
+    if(virtualMemory->currentSize != virtualMemory->maxSize){
         frame = virtualMemory->currentSize; //Grab currentFrame number and increase by one;
         addNewNode(pageNum,pid, frame, virtualMemory);
 
@@ -45,16 +45,16 @@ int addToVirtualMemory(int pageNum,int pid, node* frameBuffer[], doubleLL* virtu
     return frame;
 }
 
-void policyUpdateLRU(node* current, doubleLL* container){
-    node* next = current->next;
-    node* previous = current->previous;
+/* void policyUpdateLRU(node* current, doubleLL* container){ */
+/*     node* next = current->next; */
+/*     node* previous = current->previous; */
 
-    next->previous = previous;
-    previous->next = next;
+/*     next->previous = previous; */
+/*     previous->next = next; */
 
-    node* top = container->head->next;
-    container->head->next = current;
-    top->previous = current;
-    current->next = top;
-    current->previous = container->head;
-}
+/*     node* top = container->head->next; */
+/*     container->head->next = current; */
+/*     top->previous = current; */
+/*     current->next = top; */
+/*     current->previous = container->head; */
+/* } */
